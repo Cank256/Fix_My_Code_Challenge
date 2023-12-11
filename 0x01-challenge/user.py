7acefd7a68960d46ef44f0780dcb555999169754
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-12-11 14:39:43 +0300

    define email property

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -10,6 +10,11 @@ class User():
         """ Documentation """
         self.__email = None
 
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+
     @email.setter
     def email(self, value):
         """ Documentation """
