0a4b494391d71a00112e49079c2b22490c5ab477
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-12-12 13:34:44 +0300

    update the area of square function

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,7 +8,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def permiter_of_my_square(self):
         return (self.width + self.height) * 2
