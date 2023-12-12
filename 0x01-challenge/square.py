3e9624290caaed19f3f0c2fb5bcbd25688fc85cf
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-12-12 13:44:26 +0300

    document square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python3
 
-class Square():
+class Square:
+    """A class representing a square.
 
-    def __init__(self, width=0, height=0):
+    Attributes:
+        width (int): The width of the square.
+        height (int): The height of the square.
+    """
+
+    def __init__(self, width, height):
+        """Initializes a new instance of the Square class.
+
+        Args:
+            width (int): The width of the square.
+            height (int): The height of the square.
+        """
         self.width = width
         self.height = height
 
-    def area_of_my_square(self):
-        """ Area of the square """
+    def area(self):
+        """Returns the area of the square."""
         return self.width * self.height
 
-    def permiter_of_my_square(self):
-        return (self.width + self.height) * 2
+    def perimeter(self):
+        """Returns the perimeter of the square."""
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """Returns a string representation of the square."""
+        return f"{self.width}/{self.height}"
 
 
 if __name__ == "__main__":
-
     s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print(s.area())
+    print(s.perimeter())
